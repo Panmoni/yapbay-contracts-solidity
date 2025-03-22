1d41fb8b273144ea53754a8f2399fafc4c7bfc30
George Donnelly <me@georgedonnelly.com>
2025-03-22 15:34:11 +0000

    update hardhat config for celo

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-ethers";
+import "@openzeppelin/hardhat-upgrades";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: "0.8.17",
+  networks: {
+    alfajores: {
+      url: "https://alfajores-forno.celo-testnet.org",
+      accounts: [process.env.PRIVATE_KEY as string],
+      chainId: 44787,
+    },
+  },
 };
 
 export default config;
