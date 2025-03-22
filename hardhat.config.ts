512bff05deb26e933cd7f80e9490ad76cfb5ba42
George Donnelly <me@georgedonnelly.com>
2025-03-22 17:28:26 +0000

    update ts, hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,16 +1,35 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomiclabs/hardhat-ethers";
+import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
-import "dotenv/config";
+import "@typechain/hardhat";
+import * as dotenv from "dotenv";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      },
+      viaIR: true
+    }
+  },
   networks: {
-    alfajores: {
-      url: "https://alfajores-forno.celo-testnet.org",
-      accounts: [process.env.PRIVATE_KEY as string],
-      chainId: 44787,
+    hardhat: {
+      chainId: 31337,
     },
+    localhost: {
+      chainId: 31337,
+    },
+    alfajores: {
+      url: process.env.NETWORK_URL || "",
+      accounts: process.env.ARBITRATOR_PRIVATE_KEY ? [process.env.ARBITRATOR_PRIVATE_KEY] : [],
+    }
+  },
+  typechain: {
+    outDir: "typechain",
+    target: "ethers-v6",
   },
 };
 
